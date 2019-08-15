c408526e12d7e60855fdbdd50e12d25e39e259fa
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-15 17:34:41 +0530

    Added update and edit routes to blog app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,26 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+        print params[:id], "\n"
+        print "Article: ", @article
     end
 
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+ 
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
     end
 
     def create
